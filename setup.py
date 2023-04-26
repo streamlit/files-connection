9a923281247924516a9e4762dbac808a68942ec3
Joshua Carroll <joshua.carroll@snowflake.com>
2023-04-26 15:52:58 -0700

    Fix python_requires in setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
-    python_requires=">=3.8.*",
+    python_requires=">=3.8",
     # Requirements
     install_requires=INSTALL_REQUIRES,
     packages=["st_files_connection"]
